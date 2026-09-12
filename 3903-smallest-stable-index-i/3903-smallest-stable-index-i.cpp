d25fbc6aa02c102586ac67650f86631c1987532f
Udit kumar Bindhani <uditkumarbindhani86@gmail.com>
2026-09-12 22:40:56 +0530

    Time: 0 ms (100%), Space: 31.2 MB (32.24%) - LeetHub

---
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int firstStableIndex(std::vector<int>& nums, int k) {
+        int n = nums.size();
+        if (n == 0) return -1;
+        
+        std::vector<int> suffix_min(n);
+        suffix_min[n - 1] = nums[n - 1];
+        
+        for (int i = n - 2; i >= 0; --i) {
+            suffix_min[i] = std::min(suffix_min[i + 1], nums[i]);
+        }
+        
+        int current_max = -1;
+        
+        for (int i = 0; i < n; ++i) {
+            current_max = std::max(current_max, nums[i]);
+            
+            if (current_max - suffix_min[i] <= k) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+};
